b163da934aa34229018e2d9aabfea379536b7c55
rony1533 <ronysobral5@gmail.com>
2025-03-23 20:47:16 -0300

    refactor: voltantod envs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: "AIzaSyAx70rgiFFMUDHueb_GZopXDOZo-HbEjTc",
+    authDomain: "meuportifolio-9bdf6.firebaseapp.com",
+    projectId: "meuportifolio-9bdf6",
+    storageBucket: "meuportifolio-9bdf6.appspot.com",
+    messagingSenderId: "1079594438724",
+    appId: "1:1079594438724:web:7c3f66ba685c479632ac11"
   }
 }
 
