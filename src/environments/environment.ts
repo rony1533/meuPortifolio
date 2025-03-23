3ff8536669bbdd2232b31b941c9ca25191497b46
rony1533 <ronysobral5@gmail.com>
2025-03-23 19:59:25 -0300

    feat: adicionando variavel de ambiente

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.seusite.com'  // Coloque aqui a URL da sua API de produção ou outras configurações
+};
